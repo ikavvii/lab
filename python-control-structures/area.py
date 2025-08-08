9c48f2622578eb851be4746d4df9cb01223e7b1a
Kavin <immkavin.ofcl@gmail.com>
2025-08-08 10:29:04 +0000

    add files

--- python-control-structures/area.py
+++ python-control-structures/area.py
@@ -1,1 +1,7 @@
-
+name = input('Enter the shape: ')
+if name == 'S':
+    length = int(input('Enter side length: '))
+    print('Area: ', length ** 2)
+elif name == 'C':
+    radius = int(input('Enter radius: ' ))
+    print('Area: ', 3.14 * radius ** 2)
