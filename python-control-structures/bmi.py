09bc40e48ca8ec2e700d0143249132c18e95d224
Kavin <immkavin.ofcl@gmail.com>
2025-08-09 18:40:19 +0530

    Add files via upload

---
+++ python-control-structures/bmi.py
@@ -0,0 +1,13 @@
+weight = float(input('Enter weight (in kg): '))
+height = float(input('Enter height (in meters)'))
+
+bmi = weight / height ** 2
+
+if bmi < 18.5 :
+    print('Underweight')
+elif bmi >= 30.0:
+    print('Obese')
+elif bmi >= 25.0:
+    print('Overweight')
+else: 
+    print('Healthy')
