933cf3d45243f6f7a39c1e9662f33858751a42e9
Kavin <immkavin.ofcl@gmail.com>
2025-08-09 04:50:06 +0000

    ship class using its serial number

---
+++ python-control-structures/ship.py
@@ -0,0 +1,10 @@
+serial_code = input('Enter serial code: ').lower()
+
+if serial_code == 'b':
+    print('Battleship')
+elif serial_code == 'c':
+    print('Cruiser')
+elif serial_code == 'd':
+    print('Destroyer')
+elif serial_code == 'f':
+    print('Frigate')
