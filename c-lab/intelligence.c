2ca7517a588347d0dedf6d25645059bc2b7488f6
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-08-19 05:51:54 +0530

    Refactor intelligence.c to improve variable initialization and output formatting

--- c-lab/intelligence.c
+++ c-lab/intelligence.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int y = 1;
-    float x = 5.5;
+    int y;
+    float x, i;
+    printf("%-5s %-7s %-10s\n", "y", "x", "i");
 
-    for (int i = y; i <= 6; i++) {
-        while (x <= 12.5) {
-            float ans = 2 + ( y + 0.5 * x);
-            printf("%.2f, ", ans);
-            x += 0.5;
+    for (y = 1; y <= 6; y++) {
+        for (x = 5.5; x <= 12.5; x += 0.5) {
+            i = 2 + ( y + 0.5 * x);
+            printf("%-5d %-7.1f %-10.2f\n", y, x, i);
         }
-        x = 5.5;
+        printf("\n");
     }
-    printf("\b\b ");
     return 0;
 }
