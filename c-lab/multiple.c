c1f3e3787729b0cffb7bd99404326d6faa33e370
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-08-21 01:00:29 +0530

    Add multiple.c

---
+++ c-lab/multiple.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, first, last, temp; // what about unsigned integer?
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Invalid input. Exiting..");
+        exit(1);
+    }
+    // printf("%d", n);
+
+    int length = floor(log10(n)) + 1;
+    // printf("%d", length);
+
+    last = n % 10;
+
+    // gets the first digit
+    first = n;
+    do {
+        first /= 10;
+    } while (first > 10);
+
+
+    // check whether first or last digit is multiple of other
+    printf("%d %d\n",first, last);
+
+    if (first % last == 0)
+        printf("The first digit %d is a multiple of last digit %d", first, last);
+    else if (last % first == 0)
+        printf("The last digit %d is a multiple of first digit %d", last, first);
+    else 
+        printf("Neither is a multiple of the other");
+
+    return 0;
+}
