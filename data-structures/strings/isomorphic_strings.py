ba429460a44bbc07d07576c3ac1c9a8c2e6da24c
Kavin <immkavin.ofcl@gmail.com>
2025-08-29 01:31:10 +0000

    add isomorphic strings

--- data-structures/strings/isomorphic_strings.py
+++ data-structures/strings/isomorphic_strings.py
@@ -1,3 +1,11 @@
 s = input('s: ')
 t = input('t: ')
 
+d = {}
+for i, c in enumerate(s):
+    if t[i] in d.values() and d[c] == t[i]: continue;
+    if c not in d: d[c] = t[i]
+    else: 
+        print("Not isomorphic") 
+        exit(0)
+else:print("Isomorphic")
