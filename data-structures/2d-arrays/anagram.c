8c27400abc91aae0c22c3501752c1bda78d85ce0
Kavin <immkavin.ofcl@gmail.com>
2025-08-29 17:12:24 +0000

    Add anagram.c to check if two strings are anagrams

---
+++ data-structures/2d-arrays/anagram.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main()
+{
+
+  char s[50], t[50];
+
+  printf("Enter two strings: ");
+  scanf("%s", s);
+  scanf("%s", t);
+
+  
+  if (strlen(s) != strlen(t) ) {
+    printf("Not an anagram.\n");
+    return 0;
+  } 
+  
+  int i;
+  int freq[26] = {0};
+  for (i = 0; s[i] != '\0'; i++)
+  {
+    freq[tolower(s[i]) - 'a']++;
+    freq[tolower(t[i])- 'a']--;
+  }
+
+  for(i = 0; i < 26; i++) {
+    if (freq[i] != 0) {
+      printf("Not an anagram.\n");
+      return  0;
+    }
+  }
+
+  printf("Anagram.\n");
+  return 0;
+}
