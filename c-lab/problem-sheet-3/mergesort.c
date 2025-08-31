82e256920385c83ce9b6735b07ff7773f25d45e6
Kavin <immkavin.ofcl@gmail.com>
2025-08-31 18:09:38 +0000

    organize work sheet 3

---
+++ c-lab/problem-sheet-3/mergesort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void divide(int *arr, int left, int right);
+
+void merge(int *arr1, int *arr2);
+
+int main () {
+
+    int *arr = {38, 27, 43, 3, 9, 82, 10};
+
+    
+
+    return 0;
+}
+
+void divide(int *arr, int left, int right) {
+    
+
+    
+    return;
+}
+
+void merge(int *arr1, int *arr2) {
+
+
+    return;
+}
