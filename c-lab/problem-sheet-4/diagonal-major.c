f698664e1adab3a7303cdf43305d0912cb0e930f
ikavvii <immkavin.ofcl@gmail.com>
2025-09-11 14:58:59 +0530

    Fix matrix dimensions and update diagonal traversal logic in diagonal-major.c

--- c-lab/problem-sheet-4/diagonal-major.c
+++ c-lab/problem-sheet-4/diagonal-major.c
@@ -2,19 +2,19 @@
 
 int main()
 {
-    int m = 4, n = 5;
-    int matrix[4][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}};
-
+    int m = 4, n = 4;
+    // int matrix[4][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}};
+    int matrix[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
     int index = 0;
 
     for (int i = 0; i < n; i++)
     {
         int row = 0;
-        int j = index;
-        while (j <= m)
+        int col = i;
+        while (col <= n - 1)
         {
-            printf("%d ", matrix[row][j]);
-            j++;
+            printf("%d ", matrix[row][col]);
+            col++;
             row++;
             if (row == m)
                 break;
@@ -23,11 +23,14 @@ int main()
         index++;
     }
 
-    for (int i = m - 1; i > 0; i--) {
-        int row = i, col=0;
-        while (col<m-i) {
-            printf("%d ", matrix[i][col]);
+    for (int i = m - 1; i > 0; i--)
+    {
+        int row = i, col = 0;
+        while (col < m - i)
+        {
+            printf("%d ", matrix[row][col]);
             row++;
+            col++;
         }
         printf("\n");
     }
