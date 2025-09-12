b9a5d0404e5b033da56ca416b055273d060fa763
ikavvii <immkavin.ofcl@gmail.com>
2025-09-12 12:07:05 +0530

    start js

---
+++ javascript/p1.js
@@ -0,0 +1,12 @@
+// write a program to print the sum of 1 to n using for loop, while loop
+
+let N = 10
+for (let i = 1; i <= N; i++) {
+    console.log(i)
+}
+
+let i = 1
+while (i <= N) {
+    console.log(i);
+    i++;
+}
