456b3f50c08e01426d0cd11f98a269a0e842c397
ikavvii <immkavin.ofcl@gmail.com>
2025-09-27 12:10:59 +0530

    null errors

--- data-structures/stack/stack.c
+++ data-structures/stack/stack.c
@@ -27,7 +27,7 @@ bool isEmpty(Stack *s)
 void push(Stack *s, int item)
 {
     if (isFull(s))
-        return NULL;
+        return;
     s->data[++(s->TOP)] = item;
     if (s->TOP == 0)
     {
@@ -37,29 +37,29 @@ void push(Stack *s, int item)
     {
         s->min[s->TOP] = item < s->min[s->TOP - 1] ? item : s->min[s->TOP - 1];
     }
-    return NULL;
+    return;
 }
 
 void pop(Stack *s)
 {
     if (isEmpty(s))
-        return NULL;
+        return;
     (s->TOP)--;
 }
 
 int top(Stack *s)
 {
-    return isEmpty(s) ? NULL : s->data[s->TOP];
+    return isEmpty(s) ?  : s->data[s->TOP];
 }
 
 int getMin(Stack *s)
 {
-    return isEmpty(s) ? NULL : s->min[s->TOP];
+    return isEmpty(s) ? (int)NULL : s->min[s->TOP];
 }
 
 int main()
 {
-    char operations[] = {"push", "push", "push", "getMin", "pop", "top", "getMin"};
+    char operations[][10] = {"push", "push", "push", "getMin", "pop", "top", "getMin"};
 
     int arguments[][1] = {{-2}, {0}, {-3}, {}, {}, {}, {}};
 
@@ -92,6 +92,17 @@ int main()
     }
 
     int expectedOutput[] = {NULL, NULL, NULL, -3, NULL, 0, -2};
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (output[i] != expectedOutput[i])
+        {
+            printf("Test case %d failed: expected %d, got %d\n", i + 1, expectedOutput[i], output[i]);
+            return 1;
+        }
+    }
+
+    printf("All test cases passed!\n");
 
     return 0;
 }
---
+++ javascript/strings/uppercase.js
@@ -0,0 +1,2 @@
+prompt = require("prompt-sync")()
+
