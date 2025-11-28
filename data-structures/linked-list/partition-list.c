f3175282cd6109c5bd7b472eaa506b7c65e0e6a7
ikavvii <immkavin.ofcl@gmail.com>
2025-11-28 15:16:05 +0530

    Implement insert and delete functions for linked list

--- data-structures/linked-list/partition-list.c
+++ data-structures/linked-list/partition-list.c
@@ -9,13 +9,48 @@ struct ListNode
 
 struct ListNode *insert(struct ListNode *head, int index, int data)
 {
+    struct ListNode *node = (struct ListNode *)malloc(sizeof(struct ListNode));
+    if (!node)
+        return NULL;
+
+    node->val = data;
+    node->next = NULL;
+
+    if (!head || index == 0)
+    {
+        node->next = head;
+        head = node;
+    }
+    else
+    {
+        struct ListNode *curr = head;
+
+        int count = 0;
+        while (curr->next != NULL && index - count > 1)
+        {
+            curr = curr->next;
+            count++;
+        }
+
+        node->next = curr->next;
+        curr->next = node;
+    }
 
     return head;
-    
 }
 
 struct ListNode *delete(struct ListNode *head, int index)
 {
+    if (!head && index < 0) return NULL;
+    else if (index == 0) {
+        struct ListNode *temp = head;
+        head=head->next;
+        free(temp);
+    }
+    else {
+        int count = 0;
+        
+    }
 
     return head;
 }
