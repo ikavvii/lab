071ed83f6c4ffd720b4f2191943ad8204ae55caf
ikavvii <immkavin.ofcl@gmail.com>
2026-02-19 16:56:07 +0530

    work java gui w/ PostgreSQL

---
+++ full-stack-epc/student-registration-main/README.md
@@ -0,0 +1,9 @@
+# student-registration
+A student registration system that runs on a graphical user interface. Data are stored in the database.
+## Database
+* PostgreSQL
+
+### Libraries used
+* [JCalendar](https://toedter.com/jcalendar/)
+* [PostgreSQL Driver](https://jdbc.postgresql.org/download.html)
+* [Lombok](https://projectlombok.org/)
---
+++ full-stack-epc/student-registration-main/src/com/pitzzahh/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pitzzahh;
+
+import com.pitzzahh.view.Main;
+
+/**
+ *
+ * @author peter john
+ */
+public class App {
+    public static void main(String[] args) {
+        java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Kolkata"));
+        new Main().run();
+    }
+}
